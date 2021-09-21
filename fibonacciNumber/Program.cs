06b1f303db6f2a885f97d69551efd1fc4ea15479
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-21 22:23:34 +0600

    Comment has been added

--- fibonacciNumber/Program.cs
+++ fibonacciNumber/Program.cs
@@ -6,20 +6,27 @@ namespace fibonacciNumber
     {
         static void Main(string[] args)
         {
+            //variables 
             int n1 = 0, n2 = 1, n3, number;
 
+            //Message to users
             Console.Write("Enter the number of elements: ");
-            number = int.Parse(Console.ReadLine());
 
+            //input from the users
+            number = int.Parse(Console.ReadLine());
+            
+            //print the first two numbers
             Console.Write(n1 + " " + n2 + " ");
 
+            //continue the program until equal to user input
             for(int i=2; i<number; ++i)
             {
                 n3 = n1 + n2;
+                //print the number
                 Console.Write(n3 + " ");
+                //swap the numbers
                 n1 = n2;
                 n2 = n3;
-
             }
 
         }
