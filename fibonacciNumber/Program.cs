137ca53e8b40749d331a42bff462044ffc568ad6
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-21 22:08:16 +0600

    Something has been changed Variables  declared and get input from users

--- fibonacciNumber/Program.cs
+++ fibonacciNumber/Program.cs
@@ -6,7 +6,14 @@ namespace fibonacciNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1 = 0, n2 = 1, n3, number;
+
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2 + " ");
+
+
         }
     }
 }
