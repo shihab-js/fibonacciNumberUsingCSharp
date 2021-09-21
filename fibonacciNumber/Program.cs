64021939df6a5d57bbd9a82ab03358a980ac3644
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-21 22:17:48 +0600

    Loop has been added

--- fibonacciNumber/Program.cs
+++ fibonacciNumber/Program.cs
@@ -13,6 +13,14 @@ namespace fibonacciNumber
 
             Console.Write(n1 + " " + n2 + " ");
 
+            for(int i=2; i<number; ++i)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
 
         }
     }
